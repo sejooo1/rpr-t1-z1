ca72338754ad2a1253576d6d32d706efb42c693b
Sead Fejzagić <sfejzagic1@etf.unsa.ba>
2021-10-18 16:56:29 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Ispis.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+public class Ispis {
+
+    public static void main(String[] args) {
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite prvi broj: ");
+        int n = ulaz.nextInt();
+        System.out.println("Unesite drugi broj: ");
+        int m = ulaz.nextInt();
+        System.out.println(n);
+        System.out.println(m);
+    }
+}
